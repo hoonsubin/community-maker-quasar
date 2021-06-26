7b220d9f60eab4ded1170b831525403be2e182ee
Hoon Kim <mail@hoonkim.me>
2021-06-26 11:38:02 +0900

    add timeline page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,45 +1,18 @@
 <template>
   <q-page padding>
-    <div
-      class="fit column wrap justify-start items-center content-stretch q-pa-md"
-    >
-      <q-intersection
-        v-for="post in postCount"
-        transition="scale"
-        :key="post"
-        class="post-item"
-      >
-        <community-post-item
-          :title="'Random Post #' + post"
-          :uploader="{
-            nickName: 'John Doe',
-            avatarSrc: 'https://cdn.quasar.dev/img/boy-avatar.png',
-          }"
-          contentSrc="https://picsum.photos/1080/720"
-          :tags="['hello', 'this is a test']"
-        />
-      </q-intersection>
-    </div>
+    <q-btn color="deep-orange" push :to="'home'">
+      <div class="row items-center no-wrap">
+        <q-icon left name="map" />
+        <div class="text-center">View<br />Timeline</div>
+      </div>
+    </q-btn>
   </q-page>
 </template>
 
 <script lang="ts">
-import CommunityPostItem from 'components/CommunityPostItem.vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { CommunityPostItem },
-  setup() {
-    // creates mock post
-    const postCount = ref(60);
-
-    return { postCount };
-  },
 });
 </script>
-<style lang="sass" scoped>
-.post-item
-  min-height: 400px
-  min-width: 50%
-</style>
