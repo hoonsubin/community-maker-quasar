f0401e0939df5bfaedbe5a163709ae38219139d2
Hoon Kim <mail@hoonkim.me>
2021-07-02 15:50:34 +0900

    Feature/adjust timeline layout (#1)
    * separated layout components
    
    * remove unused style
    
    * add timeline page
    
    * remove unused vars
    
    * add base login modal

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,43 +1,38 @@
 <template>
   <q-page padding>
-    <div class="column wrap justify-start items-center content-stretch">
-      <q-intersection
-        v-for="post in postCount"
-        :key="post"
-        class="example-item"
-      >
-        <community-post-item
-          :title="'Random Post #' + post"
-          :uploader="{
-            nickName: 'John Doe',
-            avatarSrc: 'https://cdn.quasar.dev/img/boy-avatar.png',
-          }"
-          contentSrc="https://picsum.photos/1080/720"
-          :tags="['hello', 'this is a test']"
-        />
-      </q-intersection>
+    <div class="q-pa-md q-gutter-sm">
+<q-btn color="deep-orange" push :to="'home'">
+      <div class="row items-center no-wrap">
+        <q-icon left name="map" />
+        <div class="text-center">View<br />Timeline</div>
+      </div>
+    </q-btn>
+    <q-btn color="deep-orange" pus @click="showLogin = true">
+      <div class="row items-center no-wrap">
+        <q-icon left name="map" />
+        <div class="text-center">Login</div>
+      </div>
+    </q-btn>
+
+    <login-modal v-model="showLogin"/>
     </div>
+
   </q-page>
 </template>
 
 <script lang="ts">
-import CommunityPostItem from 'components/CommunityPostItem.vue';
 import { defineComponent, ref } from 'vue';
+import LoginModal from '../components/LoginModal.vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { CommunityPostItem },
-  setup() {
-    // creates mock post
-    const postCount = ref(60);
-
-    return { postCount };
+  components: {
+    LoginModal
   },
+  setup(){
+    return {
+      showLogin: ref(false),
+    }
+  }
 });
 </script>
-<style lang="sass" scoped>
-.example-item
-  min-height: 300px
-  min-width: 300px
-  margin: 0.5rem
-</style>
---
+++ src/pages/TimelinePage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page padding>
+    <div
+      class="fit column wrap justify-start items-center content-stretch q-pa-md"
+    >
+      <q-intersection
+        v-for="post in postCount"
+        transition="scale"
+        :key="post"
+        class="post-item"
+      >
+        <community-post-item
+          :title="'Random Post #' + post"
+          :uploader="{
+            nickName: 'John Doe',
+            avatarSrc: 'https://cdn.quasar.dev/img/boy-avatar.png',
+          }"
+          contentSrc="https://picsum.photos/1080/720"
+          :tags="['hello', 'this is a test']"
+        />
+      </q-intersection>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import CommunityPostItem from 'components/CommunityPostItem.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'TimelinePage',
+  components: { CommunityPostItem },
+  setup() {
+    // creates mock post
+    const postCount = ref(60);
+
+    return { postCount };
+  },
+});
+</script>
+<style lang="sass" scoped>
+.post-item
+  min-height: 400px
+  min-width: 500px
+</style>
